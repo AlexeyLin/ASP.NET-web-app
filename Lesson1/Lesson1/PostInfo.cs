e436732697811b911603943e001d8e139bfa45f7
AlexeyLin <aleksey.lin@gmail.com>
2021-08-05 19:44:17 +0300

    update

---
+++ Lesson1/Lesson1/PostInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    class PostInfo
+    {
+        public string userId { get; set; }
+
+        public string id { get; set; }
+
+        public string title { get; set; }
+
+        public string body { get; set; }
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Lesson1
 {
     class Program
     {
-        static void Main(string[] args)
+        private readonly HttpClient client = new HttpClient();
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var file = Environment.CurrentDirectory;
+            for (var idPost = 4; idPost < 14; idPost++)
+            {
+                var post = await GetPostInfo(idPost);
+                await File.ReadAllTextAsync(File, post.ToString() + Environment.NewLine);
+            }
+
+            Console.ReadKey();
+        }
+
+        public async Task<PostInfo> GetPostInfo(int id)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"https://jsonplaceholder.typicode.com/posts/{id}");
+            try
+            {
+                HttpResponseMessage response = await client.SendAsync(httpRequest);
+
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<PostInfo>(responseStream);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
